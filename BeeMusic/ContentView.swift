d76ada79671b10071c359eb43ba74fd8a9e33140
haoshuai <haoshuai9291@163.com>
2020-12-17 18:26:43 +0800

    配置代码

--- BeeMusic/ContentView.swift
+++ BeeMusic/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+struct SongListRowView:View  {
+    
+    var image: String
+    
+    var name: String
+    
+    var author: String
+    
+    var body: some View {
+        
+        HStack {
+            
+            Image(image)
+                .resizable()
+                .frame(width: 50, height: 50, alignment: .center)
+                .cornerRadius(25)
+            
+            VStack(alignment: .leading) {
+                Text(author)
+                    .font(.title2)
+                Text(name)
+                    .font(.body)
+            }
+            
+        }
+        
+    }
+    
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                NavigationLink(destination: SongContenView()) {
+                    SongListRowView(image: "ty", name: "Bloom", author: "戳爷")
+                }
+            }
+        }
+        .navigationBarTitle("歌曲列表", displayMode: .inline)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
--- BeeMusic/SongContenView.swift
+++ BeeMusic/SongContenView.swift
@@ -7,9 +7,72 @@
 
 import SwiftUI
 
+
+struct Blur: UIViewRepresentable {
+    
+    var style: UIBlurEffect.Style = .systemMaterial
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+    
+}
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
+        UIVisualEffectView()
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) {
+        uiView.effect = effect
+    }
+}
+
 struct SongContenView: View {
+
+    @State private var isAnimating = false
+
+    @State private var showProgress = false
+    
+    var foreverAnimation: Animation {
+        Animation.linear(duration: 2.0)
+            .repeatForever(autoreverses: false)
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            ZStack {
+
+                Image("ty")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                
+                VisualEffectView(effect: UIBlurEffect(style: .light))
+                    .edgesIgnoringSafeArea(.all)
+
+                Image("ty")
+//                    .resizable()
+//                    .frame(width: 230, height: 230, alignment: .center)
+                    .rotationEffect(Angle(degrees: self.isAnimating ? 360 : 0.0))
+//                    .animation(self.isAnimating ? foreverAnimation : .none)
+                    .onAppear{
+                        self.isAnimating = true
+                    }
+            }
+
+            
+        }
+        .navigationBarTitle("Navigation", displayMode: .inline)
+        
+        
+
     }
 }
 
