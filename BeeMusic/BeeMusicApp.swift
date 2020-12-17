eeb725005b275c9b6ca6161dc6acf9761b891f78
haoshuai <haoshuai9291@163.com>
2020-12-17 16:04:20 +0800

    Initial Commit

---
+++ BeeMusic/BeeMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeMusicApp.swift
+//  BeeMusic
+//
+//  Created by flqy on 2020/12/17.
+//
+
+import SwiftUI
+
+@main
+struct BeeMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeeMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeeMusic
+//
+//  Created by flqy on 2020/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
