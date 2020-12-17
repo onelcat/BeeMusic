1e6e34b55fc7b7d545c31732b6d870c49198d511
haoshuai <haoshuai9291@163.com>
2020-12-17 18:24:47 +0800

    init

---
+++ BeeMusic/SongContenView.swift
@@ -0,0 +1,20 @@
+//
+//  SongContenView.swift
+//  BeeMusic
+//
+//  Created by flqy on 2020/12/17.
+//
+
+import SwiftUI
+
+struct SongContenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SongContenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SongContenView()
+    }
+}
---
+++ BeeMusic/SongListModel.swift
@@ -0,0 +1,8 @@
+//
+//  SongListModel.swift
+//  BeeMusic
+//
+//  Created by flqy on 2020/12/17.
+//
+
+import Foundation
